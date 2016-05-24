7ed6cef8787de72344cd97e0ad2a3de0fb5791f1
Oyedotun Oyesanmi <dotman14@users.noreply.github.com>
2016-05-24 09:46:05 -0400

    Update caesar_cipher.cpp

--- caesar_cipher.cpp
+++ caesar_cipher.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <iterator>
 #include <map>
-using namespace std;
 
 const int ALPHABET_SIZE = 26;
 
@@ -14,7 +13,7 @@ const int ALPHABET_SIZE = 26;
  *  @param map<char,char> alphaCipher  Map pairing standard alphabet with their corresponding cipher.
  *  @param shift                       This is the number of left rotation to be made by the program
  */
-void prepareCipherText(vector<char> &alphabet, vector<char> &cipher, map<char, char> &alphaCipher, int shift);
+void prepareCipherText(std::vector<char> &alphabet, std::vector<char> &cipher, std::map<char, char> &alphaCipher, int shift);
 
 /**
  *  This function reverses the KEY:VALUE pair in a map.
@@ -25,51 +24,51 @@ void prepareCipherText(vector<char> &alphabet, vector<char> &cipher, map<char, c
  *  @param map<char,char> alphaCipher  Map pairing standard alphabet with their corresponding cipher.
  *  @param map<char,char> reverse      Reverse paring for alphaCipher map.
  */
-void reverseMap(map<char, char> &alphaCipher, map<char, char> &reverse);
+void reverseMap(std::map<char, char> &alphaCipher, std::map<char, char> &reverse);
 
 /**
  *  This function reads string from getline and returns it to either enryption/decryption
  *
  *  @return string
  */
-string getString();
+std::string getString();
 
 int main(int argc, const char * argv[]) {
     
     int shift;
-    string test;
+    std::string test;
     char response;
     
-    cout << "Encrypt(e,E) / Decrypt(d, D): ";
-    cin >> response;
+    std::cout << "Encrypt(e,E) / Decrypt(d, D): ";
+    std::cin >> response;
     
-    cout << "Shift alphabet by how many digits: ";
-    cin  >> shift;
+    std::cout << "Shift alphabet by how many digits: ";
+    std::cin  >> shift;
     
-    vector<char> alphabets(ALPHABET_SIZE), cipher(ALPHABET_SIZE);
-    map<char, char> alphaCipher, reverse;
+    std::vector<char> alphabets(ALPHABET_SIZE), cipher(ALPHABET_SIZE);
+    std::map<char, char> alphaCipher, reverse;
     
     prepareCipherText(alphabets, cipher, alphaCipher, shift);
     
     if(response == 'e' || response == 'E'){
          test = getString();
         for (int i = 0; i < test.size(); i++)
-            cout << alphaCipher.find(test[i])->second;
+            std::cout << alphaCipher.find(test[i])->second;
     }
     
     else if (response == 'd' || response == 'D'){
         reverseMap(alphaCipher, reverse);
         test = getString();
         for (int i = 0; i < test.size(); i++)
-            cout << reverse.find(test[i])->second;
+            std::cout << reverse.find(test[i])->second;
     }
     else
-        cout << "Wrong response. Try again";
+        std::cout << "Wrong response. Try again";
     
     return 0;
 }
 
-void prepareCipherText(vector<char> &alphabets, vector<char> &cipher, map<char, char> &alphaCipher, int shift)
+void prepareCipherText(std::vector<char> &alphabets, std::vector<char> &cipher, std::map<char, char> &alphaCipher, int shift)
 {
     for(int i = 0; i < 26; i++){
         alphabets.push_back((char)(97 + i));
@@ -90,19 +89,19 @@ void prepareCipherText(vector<char> &alphabets, vector<char> &cipher, map<char, 
         alphaCipher.insert ( std::pair<char,char>(' ',' ') );
     }
     else
-        cout << "Size is different";
+        std::cout << "Size is different";
 }
 
-void reverseMap(map<char, char> &alphaCipher, map<char, char> &reverse){
-    for (map<char, char>::iterator iter = alphaCipher.begin(); iter != alphaCipher.end(); ++iter)
+void reverseMap(std::map<char, char> &alphaCipher, std::map<char, char> &reverse){
+    for (std::map<char, char>::iterator iter = alphaCipher.begin(); iter != alphaCipher.end(); ++iter)
         reverse.insert ( std::pair<char,char>(iter->second, iter->first) );
 }
 
-string getString(){
-    string clearText;
-    cin.ignore();
-    cout << "Enter text you want to translate: ";
-    getline(cin, clearText);
+std::string getString(){
+    std::string clearText;
+    std::cin.ignore();
+    std::cout << "Enter text you want to translate: ";
+    std::getline(std::cin, clearText);
     
     return clearText;
 }
