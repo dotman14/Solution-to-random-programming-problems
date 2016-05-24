9fda1e396e86d722c8535bc9b490bb941d868317
Oyedotun Oyesanmi <dotman14@users.noreply.github.com>
2016-05-24 09:41:37 -0400

    Update product.cpp

--- inventory/product.cpp
+++ inventory/product.cpp
@@ -1,12 +1,11 @@
 #include "product.hpp"
 #include <iostream>
 #include <string>
-using namespace std;
 
 Products::Products() : id("UNKNOWN"), price(0.00), quantity(0) {};
-Products::Products(string id, float price, int quantity) : id(id), price(price), quantity(quantity) {};
+Products::Products(std::string id, float price, int quantity) : id(id), price(price), quantity(quantity) {};
 
-string Products::getId() const{
+std::string Products::getId() const{
     return id;
 };
 
