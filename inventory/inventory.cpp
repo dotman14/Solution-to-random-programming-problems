9fcdaa72978f5caf79be08f9924b0b5d361223a1
Oyedotun Oyesanmi <dotman14@users.noreply.github.com>
2016-05-24 09:38:39 -0400

    Update inventory.cpp
    Removed namespace std

--- inventory/inventory.cpp
+++ inventory/inventory.cpp
@@ -1,8 +1,6 @@
 #include "inventory.hpp"
 #include <iostream>
 #include <string>
-using namespace std;
-
 
 Inventory::Inventory() : productInventory(0){};
 
@@ -10,10 +8,10 @@ void Inventory::insert(Products &prod){
     if(!exist(prod.getId()))
         productInventory.push_back(prod);
     else
-        cout << "This product exist in inventory";
+        std::cout << "This product exist in inventory";
 }
 
-bool Inventory::exist(string id) const{
+bool Inventory::exist(std::string id) const{
     for(int i = 0; i < productInventory.size(); i++){
         if(productInventory.at(i).getId() == id)
             return true;
@@ -22,10 +20,10 @@ bool Inventory::exist(string id) const{
 }
 
 void Inventory::show() const{
-    cout << "Size of Inventory is " << productInventory.size() << " \n" ;
+    std::cout << "Size of Inventory is " << productInventory.size() << " \n" ;
     for (int i = 0; i < productInventory.size(); i++) {
-        cout << productInventory.at(i).getId() << " " << productInventory.at(i).getPrice() << " " << productInventory.at(i).getQuantity();
-        cout << endl;
+        std::cout << productInventory.at(i).getId() << " " << productInventory.at(i).getPrice() << " " << productInventory.at(i).getQuantity();
+        std::cout << endl;
     }
 }
 
@@ -37,21 +35,21 @@ int Inventory::countQuantity() const{
     return total;
 }
 
-void Inventory::increaseQuantity(string id, int quantity){
+void Inventory::increaseQuantity(std::string id, int quantity){
     if(!exist(id))
-        cout << "We do not have \"" << id << "\" in stock.";
+        std::cout << "We do not have \"" << id << "\" in stock.";
     for(int i = 0; i < productInventory.size(); i++){
         if(productInventory.at(i).getId() == id && (quantity >= 0))
             productInventory.at(i).setQuantity(productInventory.at(i).getQuantity() + quantity);
     }
 }
 
-void Inventory::decreaseQuantity(string id, int quantity){
+void Inventory::decreaseQuantity(std::string id, int quantity){
     if(!exist(id))
-        cout << "We do not have \"" << id << "\" in stock.";
+        std::cout << "We do not have \"" << id << "\" in stock.";
     for(int i = 0; i < productInventory.size(); i++){
         if(productInventory.at(i).getId() == id && (productInventory.at(i).getQuantity() - quantity < 0))
-            cout << "Quantity of \"" << id << "\" is very low";
+            std::cout << "Quantity of \"" << id << "\" is very low";
         
         else if(productInventory.at(i).getId() == id && (productInventory.at(i).getQuantity() - quantity >= 0))
             productInventory.at(i).setQuantity(productInventory.at(i).getQuantity() - quantity);
