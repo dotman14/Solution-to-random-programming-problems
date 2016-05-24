738c938083d46cd351e3ea60c11ef57302e10884
Oyedotun Oyesanmi <dotman14@users.noreply.github.com>
2016-05-24 09:48:36 -0400

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -2,20 +2,18 @@
 #include <algorithm>
 #include <string>
 
-using namespace std;
-
 int main(int argc, const char * argv[]) {
     
-    string str, rStr;
+    std::string str, rStr;
     
-    cout << "Enter String :\n";
-    cin >> str;
+    std::cout << "Enter String :\n";
+    std::cin >> str;
     
-    transform(str.begin(), str.end(), str.begin(), ::tolower);
+    std::transform(str.begin(), str.end(), str.begin(), ::tolower);
     rStr = str;
-    reverse(begin(str), end(str));
+    std::reverse(begin(str), end(str));
     
-    (rStr == str) ? cout << "Word is palindrone\n" : cout << "Word is not palindrone\n";
+    (rStr == str) ? std::cout << "Word is palindrone\n" : std::cout << "Word is not palindrone\n";
     
     return 0;
 }
